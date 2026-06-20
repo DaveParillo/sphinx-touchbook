3e8eee71dd885ee95133ad749de624005564a9b4
DaveParillo <DaveParillo@users.noreply.github.com>
2026-06-20 09:25:43 -0700

    fix github pages config

--- docs/conf.py
+++ docs/conf.py
@@ -8,14 +8,6 @@ extensions = ["sphinx_touchbook"]
 language = "en"
 html_theme = "sphinx_nefertiti"
 html_theme_options = {
-    # ... other options ...
-    'pygments_light_style': 'solarized-light',
-    'pygments_dark_style': 'solarized-dark'
+    "pygments_light_style": "solarized-light",
+    "pygments_dark_style": "solarized-dark",
 }
-
-# import sphinx_bootstrap_theme
-# html_theme = "bootstrap"
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-#html_theme = "pydata_sphinx_theme"
-# pygments_style = "a11y-high-contrast-light"
-# pygments_dark_style = "a11y-high-contrast-dark"
